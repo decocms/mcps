d33f758c70ec06ae01b3fd242863676079a5c89b
Marcos Candeia <marrcooos@gmail.com>
2026-01-08 22:44:39 -0300

    Fixes build-mcp pkg
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- scripts/build-mcp.ts
+++ scripts/build-mcp.ts
@@ -1,30 +1,31 @@
 import { $ } from "bun";
 
-const SITE_NAME_TO_MCP_FOLDER_OVERRIDES = {
-  vibemcp: "mcp-studio",
+const PKG_NAME_OVERRIDES = {
+	vibemcp: "mcp-studio",
+	openrouter: "@decocms/openrouter",
 };
 
-const getMcpFolder = (siteName: string) => {
-  return SITE_NAME_TO_MCP_FOLDER_OVERRIDES[siteName] || siteName;
+const getMcpPkgName = (siteName: string) => {
+	return PKG_NAME_OVERRIDES[siteName] || siteName;
 };
 
 const siteName = process.env.DECO_SITE_NAME;
 
 if (!siteName) {
-  console.error("❌ Error: DECO_SITE_NAME environment variable is not set");
-  process.exit(1);
+	console.error("❌ Error: DECO_SITE_NAME environment variable is not set");
+	process.exit(1);
 }
 
 console.log(`Building MCP for site name ${siteName}`);
 
-const folderToBuild = getMcpFolder(siteName);
+const pkgToBuild = getMcpPkgName(siteName);
 
-if (!folderToBuild) {
-  console.error(
-    `❌ Error building MCP: package with name ${siteName} not found`,
-  );
-  process.exit(1);
+if (!pkgToBuild) {
+	console.error(
+		`❌ Error building MCP: package with name ${siteName} not found`,
+	);
+	process.exit(1);
 }
 
-await $`bun run --filter=${folderToBuild} build`;
-await $`mv ./${folderToBuild}/dist ./dist`;
+await $`bun run --filter=${pkgToBuild} build`;
+await $`mv ./${pkgToBuild}/dist ./dist`;
