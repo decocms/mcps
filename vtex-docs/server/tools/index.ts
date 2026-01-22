eead9dc33637029a061ae60a076b3f180465d465
igoramf <igoraaf1@gmail.com>
2026-01-22 16:00:27 -0300

    Update gitignore, chunk size, supabase config and remove assistant tool

--- vtex-docs/server/tools/index.ts
+++ vtex-docs/server/tools/index.ts
@@ -2,7 +2,6 @@
  * Central export point for all tools organized by domain.
  */
 import { userTools } from "@decocms/mcps-shared/tools/user";
-import { vtexDocsAssistant } from "./assistant.ts";
 import { vtexDocsSearch } from "./search.ts";
 
-export const tools = [...userTools, vtexDocsAssistant, vtexDocsSearch];
+export const tools = [...userTools, vtexDocsSearch];
