758e3c7944444723b1859264b7b904775038ab7e
guitavano <tavano62@gmail.com>
2026-06-23 14:43:23 -0300

    Revert "Revert "feat(vtex): orders timeline MCP App UI + admin analytics tren…"
    This reverts commit 6581d321a7fe05b821fcc738386db6381381d444.

---
+++ vtex/server/constants.ts
@@ -0,0 +1,5 @@
+/**
+ * Shared resource URI for the VTEX MCP App UI bundle.
+ * Tools that render UI point their `_meta.ui.resourceUri` here.
+ */
+export const VTEX_RESOURCE_URI = "ui://vtex/dashboard";
